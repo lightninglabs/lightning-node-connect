58d67bcaf6dd8b81900cdaab39aa83d7cd13a5bb
Elle Mouton <elle.mouton@gmail.com>
2022-03-17 13:55:49 +0200

    cmd/wasm-client: create a wasmClient object
    In this commit, we remove the use of global variables in the wasm client
    and instead add the variables to a wasmClient object.

--- cmd/wasm-client/main.go
+++ cmd/wasm-client/main.go
@@ -1,3 +1,4 @@
+//go:build js
 // +build js
 
 package main
@@ -5,6 +6,7 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"fmt"
 	"net/http"
 	"os"
 	"runtime/debug"
@@ -31,53 +33,32 @@ import (
 type stubPackageRegistration func(map[string]func(context.Context,
 	*grpc.ClientConn, string, func(string, error)))
 
-var (
-	cfg     = config{}
-	lndConn *grpc.ClientConn
-
-	registry = make(map[string]func(context.Context, *grpc.ClientConn,
-		string, func(string, error)))
-
-	registrations = []stubPackageRegistration{
-		lnrpc.RegisterLightningJSONCallbacks,
-		lnrpc.RegisterStateJSONCallbacks,
-		autopilotrpc.RegisterAutopilotJSONCallbacks,
-		chainrpc.RegisterChainNotifierJSONCallbacks,
-		invoicesrpc.RegisterInvoicesJSONCallbacks,
-		routerrpc.RegisterRouterJSONCallbacks,
-		signrpc.RegisterSignerJSONCallbacks,
-		verrpc.RegisterVersionerJSONCallbacks,
-		walletrpc.RegisterWalletKitJSONCallbacks,
-		watchtowerrpc.RegisterWatchtowerJSONCallbacks,
-		wtclientrpc.RegisterWatchtowerClientJSONCallbacks,
-		looprpc.RegisterSwapClientJSONCallbacks,
-		poolrpc.RegisterTraderJSONCallbacks,
-	}
-)
+var registrations = []stubPackageRegistration{
+	lnrpc.RegisterLightningJSONCallbacks,
+	lnrpc.RegisterStateJSONCallbacks,
+	autopilotrpc.RegisterAutopilotJSONCallbacks,
+	chainrpc.RegisterChainNotifierJSONCallbacks,
+	invoicesrpc.RegisterInvoicesJSONCallbacks,
+	routerrpc.RegisterRouterJSONCallbacks,
+	signrpc.RegisterSignerJSONCallbacks,
+	verrpc.RegisterVersionerJSONCallbacks,
+	walletrpc.RegisterWalletKitJSONCallbacks,
+	watchtowerrpc.RegisterWatchtowerJSONCallbacks,
+	wtclientrpc.RegisterWatchtowerClientJSONCallbacks,
+	looprpc.RegisterSwapClientJSONCallbacks,
+	poolrpc.RegisterTraderJSONCallbacks,
+}
 
 func main() {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Debugf("Recovered in f: %v", r)
+			fmt.Printf("Recovered in f: %v", r)
 			debug.PrintStack()
 		}
 	}()
 
-	// Setup JS callbacks.
-	js.Global().Set("wasmClientIsReady", js.FuncOf(wasmClientIsReady))
-	js.Global().Set(
-		"wasmClientConnectServer", js.FuncOf(wasmClientConnectServer),
-	)
-	js.Global().Set(
-		"wasmClientIsConnected", js.FuncOf(wasmClientIsConnected),
-	)
-	js.Global().Set("wasmClientDisconnect", js.FuncOf(wasmClientDisconnect))
-	js.Global().Set("wasmClientInvokeRPC", js.FuncOf(wasmClientInvokeRPC))
-	for _, registration := range registrations {
-		registration(registry)
-	}
-
 	// Parse command line flags.
+	cfg := config{}
 	parser := flags.NewParser(&cfg, flags.Default)
 	parser.SubcommandsOptional = true
 
@@ -103,24 +84,55 @@ func main() {
 		exit(err)
 	}
 
+	wc := newWasmClient(&cfg)
+
+	// Setup JS callbacks.
+	js.Global().Set("wasmClientIsReady", js.FuncOf(wc.IsReady))
+	js.Global().Set("wasmClientConnectServer", js.FuncOf(wc.ConnectServer))
+	js.Global().Set("wasmClientIsConnected", js.FuncOf(wc.IsConnected))
+	js.Global().Set("wasmClientDisconnect", js.FuncOf(wc.Disconnect))
+	js.Global().Set("wasmClientInvokeRPC", js.FuncOf(wc.InvokeRPC))
+
+	for _, registration := range registrations {
+		registration(wc.registry)
+	}
+
 	log.Debugf("WASM client ready for connecting")
 
 	select {
 	case <-shutdownInterceptor.ShutdownChannel():
 		log.Debugf("Shutting down WASM client")
-		_ = wasmClientDisconnect(js.ValueOf(nil), nil)
+		_ = wc.Disconnect(js.ValueOf(nil), nil)
 		log.Debugf("Shutdown of WASM client complete")
 	}
 }
 
-func wasmClientIsReady(_ js.Value, _ []js.Value) interface{} {
+type wasmClient struct {
+	cfg *config
+
+	lndConn *grpc.ClientConn
+
+	registry map[string]func(context.Context, *grpc.ClientConn,
+		string, func(string, error))
+}
+
+func newWasmClient(cfg *config) *wasmClient {
+	return &wasmClient{
+		cfg:     cfg,
+		lndConn: nil,
+		registry: make(map[string]func(context.Context,
+			*grpc.ClientConn, string, func(string, error))),
+	}
+}
+
+func (w *wasmClient) IsReady(_ js.Value, _ []js.Value) interface{} {
 	// This will always return true. So as soon as this method is called
 	// successfully the JS part knows the WASM instance is fully started up
 	// and ready to connect.
 	return js.ValueOf(true)
 }
 
-func wasmClientConnectServer(_ js.Value, args []js.Value) interface{} {
+func (w *wasmClient) ConnectServer(_ js.Value, args []js.Value) interface{} {
 	if len(args) != 3 {
 		return js.ValueOf("invalid use of wasmClientConnectServer, " +
 			"need 3 parameters: server, isDevServer, pairingPhrase")
@@ -140,7 +152,7 @@ func wasmClientConnectServer(_ js.Value, args []js.Value) interface{} {
 	}
 
 	var err error
-	lndConn, err = mailboxRPCConnection(mailboxServer, pairingPhrase)
+	w.lndConn, err = mailboxRPCConnection(mailboxServer, pairingPhrase)
 	if err != nil {
 		exit(err)
 	}
@@ -150,13 +162,13 @@ func wasmClientConnectServer(_ js.Value, args []js.Value) interface{} {
 	return nil
 }
 
-func wasmClientIsConnected(_ js.Value, _ []js.Value) interface{} {
-	return js.ValueOf(lndConn != nil)
+func (w *wasmClient) IsConnected(_ js.Value, _ []js.Value) interface{} {
+	return js.ValueOf(w.lndConn != nil)
 }
 
-func wasmClientDisconnect(_ js.Value, _ []js.Value) interface{} {
-	if lndConn != nil {
-		if err := lndConn.Close(); err != nil {
+func (w *wasmClient) Disconnect(_ js.Value, _ []js.Value) interface{} {
+	if w.lndConn != nil {
+		if err := w.lndConn.Close(); err != nil {
 			log.Errorf("Error closing RPC connection: %v", err)
 		}
 	}
@@ -164,13 +176,13 @@ func wasmClientDisconnect(_ js.Value, _ []js.Value) interface{} {
 	return nil
 }
 
-func wasmClientInvokeRPC(_ js.Value, args []js.Value) interface{} {
+func (w *wasmClient) InvokeRPC(_ js.Value, args []js.Value) interface{} {
 	if len(args) != 3 {
 		return js.ValueOf("invalid use of wasmClientInvokeRPC, " +
 			"need 3 parameters: rpcName, request, callback")
 	}
 
-	if lndConn == nil {
+	if w.lndConn == nil {
 		return js.ValueOf("RPC connection not ready")
 	}
 
@@ -178,7 +190,7 @@ func wasmClientInvokeRPC(_ js.Value, args []js.Value) interface{} {
 	requestJSON := args[1].String()
 	jsCallback := args[len(args)-1:][0]
 
-	method, ok := registry[rpcName]
+	method, ok := w.registry[rpcName]
 	if !ok {
 		return js.ValueOf("rpc with name " + rpcName + " not found")
 	}
@@ -194,7 +206,7 @@ func wasmClientInvokeRPC(_ js.Value, args []js.Value) interface{} {
 			}
 		}
 		ctx := context.Background()
-		method(ctx, lndConn, requestJSON, cb)
+		method(ctx, w.lndConn, requestJSON, cb)
 		<-ctx.Done()
 	}()
 	return nil
@@ -202,6 +214,9 @@ func wasmClientInvokeRPC(_ js.Value, args []js.Value) interface{} {
 }
 
 func exit(err error) {
-	log.Debugf("Error running wasm client: %v", err)
+	// We use the fmt package for this error statement here instead of the
+	// logger so that we can use this exit function before the logger has
+	// been initialised since this would result in a panic.
+	fmt.Printf("Error running wasm client: %v\n", err)
 	os.Exit(1)
 }
